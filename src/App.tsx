1636019e9cab41542ab155f410a45841b6dce76f
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2024-01-02 17:20:19 -0800

    Added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import { useEffect, useState } from "react";
 import { Page, getData } from "./getData";
 import { RenderPages } from "./RenderPages";
 
+import ReactGA from 'react-ga4';
+
 const promise = getData();
 
 export function App() {
   const [value, setValue] = useState<Page[] | undefined>(undefined);
   const [resolved, setResolved] = useState(false);
   
+  useEffect(() => {
+    ReactGA.initialize('G-YSNQML280F');
+    ReactGA.send("pageview");
+  }, []);
+
   useEffect(() => {
     promise.then(setValue);
     setResolved(true);
