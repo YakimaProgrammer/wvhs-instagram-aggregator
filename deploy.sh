3b79ccd4a14f4b4a865d1e613455d13f148255b0
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2024-02-06 19:27:54 -0800

    Updated the deploy script
    Now
    - Restarts the post server
    - Deploys the story downloader
    
    Also
    - Added some comments to explain each step

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
+#Build the webclient
 ( cd client && npm run build && cd build && scp -r * hp:/var/www/wvhs-instagram.magnusfulton.com ) &
+
+#Build the server
 ( cd server && npm run build && cd dist && scp -r * hp:/opt/instagram-aggregator ) &
 
+#Copy over the download script
+scp story-downloader/downloader.py hp:/opt/story-downloader &
+
 wait
+
+#Restart the server
+ssh hp -C "sudo systemctl restart instagram-aggregator.service"
