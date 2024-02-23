0326c232365df3085dfcebeafd2c94a6ef6c7748
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2024-02-22 21:14:53 -0800

    Now uses the const file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import { Account } from "./shared/loadData";
 import { fs } from "./shared/fs-poly/native";
+import { API } from "./shared/const";
 
 const app: Express = express();
 const port = 3000;
@@ -12,7 +13,7 @@ if (!!process.env.DATA) {
   process.exit(1);
 }
 
-const account = new Account(fs, "https://api.magnusfulton.com/instagram/");
+const account = new Account(fs, API);
 
 app.get("/instagram/api", (req: Request, res: Response) => {
   account.getPosts().then(posts => res.type("application/json").send(JSON.stringify(posts)));
