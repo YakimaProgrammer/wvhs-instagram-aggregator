d98d6b54b066dc9b8b94e9b6b7e7f4554c3eb162
Magnus Fulton (@YakimaProgrammer) <magnus@magnusfulton.com>
2024-09-06 10:29:58 -0700

    Modified the post loading logic
    <PostsLoader /> now uses an Option<_> to track if posts have
    been loaded or not, rather than checking if the Vec<Post>
    is empty or not. This allows for the possibility that there
    could be an empty list of posts without displaying the
    <Loading /> component forever.

--- website/src/components/posts.rs
+++ website/src/components/posts.rs
@@ -10,16 +10,22 @@ pub struct PostsProps {
 /// Displays a collection of `Post`s
 #[function_component(Posts)]
 pub fn posts(PostsProps { posts }: &PostsProps) -> Html {
-  let posts = {
-    posts
+  if posts.is_empty() {
+    html! {
+      <h2><i>{ "It looks like there aren't any posts right now. Check back later!" }</i></h2>
+    }
+  } else {
+    let posts = {
+      posts
       .iter()
       .map(|post| {
           html! {<div key={post.time}>{ format!("This post was made by {}. It reads {}", post.profile_name, post.caption) }</div>}
       })
       .collect::<Html>()
-  };
+    };
 
-  html! {
-    <>{posts}</>
+    html! {
+      <>{posts}</>
+    }
   }
 }
--- website/src/components/posts_loader.rs
+++ website/src/components/posts_loader.rs
@@ -10,39 +10,38 @@ static API_ENDPOINT: &str = include_str!("../.API_ENDPOINT");
 /// A component responsible for loading posts
 #[function_component(PostsLoader)]
 pub fn posts_loader() -> Html {
-  let posts: UseStateHandle<Option<Arc<Vec<Post>>>> = use_state(|| Some(Arc::new(vec![])));
+  let posts: UseStateHandle<Result<Option<Arc<Vec<Post>>>, Error>> = use_state(|| Ok(None));
   {
     let posts = posts.clone();
     use_effect_with((), move |_| {
       let posts = posts.clone();
       wasm_bindgen_futures::spawn_local(async move {
         // Attempt to fetch the posts
-        let result: Result<Vec<Post>, Error> = async {
+        let result: Result<Option<Arc<Vec<Post>>>, Error> = async {
           let response = Request::get(API_ENDPOINT).send().await?;
           let posts: Vec<Post> = response.json().await?;
-          Ok(posts)
+          Ok(Some(Arc::new(posts)))
         }
         .await;
-        posts.set(result.ok().map(|v| Arc::new(v)));
+        posts.set(result);
       });
       || ()
     });
   }
 
   match &*posts {
-    Some(vec) => {
-      if vec.is_empty() {
-        html! {
-          <Loading />
-        }
-      } else {
-        html! {
-          <Posts posts={vec.clone()} />
-        }
+    Ok(Some(vec)) => {
+      html! {
+        <Posts posts={vec.clone()} />
+      }
+    }
+    Ok(None) => {
+      html! {
+        <Loading />
       }
     }
 
-    None => {
+    Err(_) => {
       html! {
         <h2>{ "Sorry, the server could not be reached. Are you connected to the internet?" }</h2>
       }
